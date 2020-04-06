badd6dbc0634c17fa23d9fb7462c44ee00285f7e
Martin Acosta <tinchoz49@gmail.com>
2020-04-06 01:15:09 -0300

    added extend

--- README.md
+++ README.md
@@ -15,7 +15,43 @@ $ npm install @geut/sodium-javascript-plus
 
 ## <a name="usage"></a> Usage
 
+```javascript
+const message = Buffer.from('some secret message')
+const ad = Buffer.from('50515253c0c1c2c3c4c5c6c7', 'hex')
+const nonce = Buffer.from('404142434445464748494a4b4c4d4e4f5051525354555657', 'hex')
+const key = Buffer.from('808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f', 'hex')
+const cipher = Buffer.alloc(message.length + sodium.crypto_aead_xchacha20poly1305_ietf_ABYTES)
+
+sodium.crypto_aead_xchacha20poly1305_ietf_encrypt(
+  cipher,
+  message,
+  ad,
+  null,
+  nonce,
+  key
+)
+
+const decrypted = Buffer.alloc(message.length)
+
+sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(
+  decrypted,
+  null,
+  cipher,
+  ad,
+  nonce,
+  key
+)
+
+console.log(message.equals(decrypted), 'same message')
 ```
+
+### You can extend your sodium instance
+
+```javascript
+const extend = require('@geut/sodium-javascript-plus/extend')
+const sodium = extend(require('sodium-javascript'))
+
+// ...
 ```
 
 ## <a name="issues"></a> Issues
---
+++ extend.js
@@ -0,0 +1,115 @@
+const Heap = require('./lib/heap')
+const loadSodiumWasm = require('./lib/load-sodium-wasm')
+
+module.exports = (sodiumJS) => {
+  const heap = new Heap()
+
+  const sodiumWasm = loadSodiumWasm(sodiumJS, heap)
+
+  sodiumJS.crypto_aead_xchacha20poly1305_ietf_encrypt = function (out, m, ad, nsec, nonce, key) {
+    const cipherLength = m.length + sodiumJS.crypto_aead_xchacha20poly1305_ietf_ABYTES
+    const _out = heap.alloc(cipherLength)
+
+    sodiumWasm.crypto_aead_xchacha20poly1305_ietf_encrypt(
+      _out,
+      null,
+      heap.set(m),
+      m.length,
+      ad ? heap.set(ad) : null,
+      ad ? ad.length : null,
+      nsec ? heap.set(nsec) : null,
+      heap.set(nonce),
+      heap.set(key)
+    )
+
+    heap.copy(out, _out, cipherLength)
+    heap.clear()
+    return cipherLength
+  }
+
+  sodiumJS.crypto_aead_xchacha20poly1305_ietf_decrypt = function (out, nsec, c, ad, nonce, key) {
+    const messageLength = c.length - sodiumJS.crypto_aead_xchacha20poly1305_ietf_ABYTES
+    const _out = heap.alloc(messageLength)
+
+    sodiumWasm.crypto_aead_xchacha20poly1305_ietf_decrypt(
+      _out,
+      null,
+      nsec ? heap.set(nsec) : null,
+      heap.set(c),
+      c.length,
+      ad ? heap.set(ad) : null,
+      ad ? ad.length : null,
+      heap.set(nonce),
+      heap.set(key)
+    )
+
+    heap.copy(out, _out, messageLength)
+    heap.clear()
+    return messageLength
+  }
+
+  sodiumJS.crypto_kx_keypair = function (pk, sk) {
+    const _pk = heap.alloc(pk.length)
+    const _sk = heap.alloc(pk.length)
+
+    sodiumWasm.crypto_kx_keypair(
+      _pk,
+      _sk
+    )
+
+    heap.copy(pk, _pk, pk.length)
+    heap.copy(sk, _sk, sk.length)
+    heap.clear()
+  }
+
+  sodiumJS.crypto_kx_seed_keypair = function (pk, sk, seed) {
+    const _pk = heap.alloc(pk.length)
+    const _sk = heap.alloc(pk.length)
+
+    sodiumWasm.crypto_kx_seed_keypair(
+      _pk,
+      _sk,
+      heap.set(seed)
+    )
+
+    heap.copy(pk, _pk, pk.length)
+    heap.copy(sk, _sk, sk.length)
+    heap.clear()
+  }
+
+  sodiumJS.crypto_kx_client_session_keys = function (clientRx, clientTx, clientPk, clientSk, serverPk) {
+    const _clientRx = heap.alloc(clientRx.length)
+    const _clientTx = heap.alloc(clientTx.length)
+
+    sodiumWasm.crypto_kx_client_session_keys(
+      _clientRx,
+      _clientTx,
+      heap.set(clientPk),
+      heap.set(clientSk),
+      heap.set(serverPk)
+    )
+
+    heap.copy(clientRx, _clientRx, clientRx.length)
+    heap.copy(clientTx, _clientTx, clientTx.length)
+    heap.clear()
+  }
+
+  sodiumJS.crypto_kx_server_session_keys = function (serverRx, serverTx, serverPk, serverSk, clientPk) {
+    const _serverRx = heap.alloc(serverRx.length)
+    const _serverTx = heap.alloc(serverTx.length)
+
+    sodiumWasm.crypto_kx_server_session_keys(
+      _serverRx,
+      _serverTx,
+      heap.set(serverPk),
+      heap.set(serverSk),
+      heap.set(clientPk)
+    )
+
+    heap.copy(serverRx, _serverRx, serverRx.length)
+    heap.copy(serverTx, _serverTx, serverTx.length)
+    heap.clear()
+  }
+
+  return sodiumJS
+}
